16f8f22c0a4e69b90fd05f1a9d0ca5275a661077
Realcat <realcat@126.com>
2021-10-09 00:26:13 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -108,8 +108,8 @@ if __name__ == "__main__":
         day = str(DateToday + timedelta(-i))
         # you can add the categories in cats
         cats = {
-        "cs":["cs.CV" "cs.RO" "cs.AI" "cs.MM"]
-        "eess":["eess.SP" "eess.IV" ],    
+        "cs":["cs.CV" "cs.RO" "cs.AI" "cs.MM"],
+        "eess":["eess.SP" "eess.IV" ]
     }
         data = get_daily_code(day,cats)
         data_all.append(data)
