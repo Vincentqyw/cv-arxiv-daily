b1b74c08a1c5fe2cf1652c631b0861aed81ce9ba
Realcat <realcat@126.com>
2022-04-19 15:25:50 +0800

    Create daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
     keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\"OR\"Loop Closure Detection\"OR\"visual place recognition\"OR\"image retrieval\""
     keywords["Keypoint Detection"]  = "\"Keypoint Detection\"OR\"Feature Descriptor\""
-    keywords["Image Matching"]      = "\"Image Matching\""
+    keywords["Image Matching"]      = "\"Image Matching\"OR\"Keypoint Matching\""
 
     for topic,keyword in keywords.items():
  
